71fac04a8d70f7e302adffba11f34e290565d3d6
srivarshikareddy@gmail.com <srivarshikareddy@gmail.com>
2025-11-28 11:12:17 +0530

    first commit

---
+++ src/main/java/com/quizapp/Nov28Application.java
@@ -0,0 +1,13 @@
+package com.quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nov28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nov28Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/quizapp/controller/QuestionController.java
@@ -0,0 +1,25 @@
+package com.quizapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quizapp.model.Question;
+import com.quizapp.service.QuestionService;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+	
+	@Autowired
+	QuestionService questionServices;
+	
+	@GetMapping("/allQuestions")
+	public List<Question> getAllQuestions() {
+		return questionService.getAllQuestions();
+	}
+
+}
